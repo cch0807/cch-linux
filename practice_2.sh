38220af7dd6364897d586ad1bdd1843219bc10a4
chchoi <ch.choi@aizen.co>
2022-09-09 16:01:05 +0900

    20220909_1

---
+++ practice_2.sh
@@ -0,0 +1,10 @@
+cat practice.txt
+head practice.txt # 기본이 10 line
+head practice.txt -n 3
+tail practice.txt
+tail practice.txt -n 3
+
+# head --help
+
+less practice.txt
+
---
+++ practice_3.sh
@@ -0,0 +1,5 @@
+# 파일 내용 검색
+
+grep --help
+
+grep 'hello' practice.txt
