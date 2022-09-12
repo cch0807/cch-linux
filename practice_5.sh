052899e75453c3a752ec3182f7ee71b7e0915be5
cch0807 <ckdgus11021@gmail.com>
2022-09-12 22:21:42 +0900

    20220912_1

--- practice_5.sh
+++ practice_5.sh
@@ -14,5 +14,26 @@ gzip filelist
 
 ls
 
+# file {파일명} -> 해당 파일이 어떤 파일인지 알려주는 명령어
 file filelist.gz
 
+# 압축 해제
+gunzip filelist.gz
+
+gzip filelist
+
+mv filelist test
+
+file test
+
+mv test filelist.gz
+
+tar --help
+
+# 압축
+tar -cfz test.tar.gz filelist.gz Linux.md practice.txt
+
+cd practice
+
+# 압축 해제
+tar -zxf ../test.tar.gz 
