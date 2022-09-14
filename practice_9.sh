fe756aa63dbadf5680cd6850286b52bf9e74c480
cch0807 <ckdgus11021@gmail.com>
2022-09-14 23:46:48 +0900

    20220914_2

---
+++ practice_9.sh
@@ -0,0 +1,28 @@
+# 파일 다루기
+
+ls -al
+
+touch hahaha # hahaha 파일 생성
+
+rm hahaha # 삭제
+
+nano testfile
+
+ls -al testfile
+
+cat testfile
+
+pwd # /home/chchoi0807/linux_study
+
+mv testfile apple 
+
+cat apple
+
+mv apple /tmp/pineapple
+
+ls /tmp
+
+cat /tmp/pineapple
+
+mv /tmp/pineapple .
+
