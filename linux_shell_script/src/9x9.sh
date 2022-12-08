bd1ca1241d972c0c6744e2f2203f71647646d298
cch0807 <ckdgus11021@gmail.com>
2022-12-08 23:42:55 +0900

    20221208_2

---
+++ linux_shell_script/src/9x9.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {2..9}
+do
+    for ((j=1; j<10; j++))
+    do
+        ((result=i*j))
+        if [ $result -lt 30 ]; then
+            echo "$i x $j = $result"
+        else
+            echo "$i x $j = ..... nope"
+        echo "$i x $j = $result"  
+        fi
+    done
+done
