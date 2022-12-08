59c301ad7a76d63257b2133d8fa9e56864b38824
cch0807 <ckdgus11021@gmail.com>
2022-12-08 22:24:40 +0900

    20221208_1

---
+++ linux_shell_script/src/while_until.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+while [ .... ]
+do
+
+done
+
+-----------------------------------------------------------------------
+
+index=0
+
+while [ $index -lt 10 ]
+do
+    echo "index is $index"
+
+    let 'index++' # ((index++)) 도 가능
+done
