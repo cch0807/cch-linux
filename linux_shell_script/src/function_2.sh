2fbfbae58d2836a5e2ef3e547e9ba6851f9df829
cch0807 <ckdgus11021@gmail.com>
2022-12-11 23:57:31 +0900

    20221211_2

---
+++ linux_shell_script/src/function_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function test1()
+{
+    if [ -z "$1" ]; then
+        echo "param1 is missing"
+    fi
+    if [ -z "$2" ]; then
+        echo "param2 is missing"
+    fi
+
+
+    param1="$1"
+    param2="$2"
+    echo "this is test1"
+    echo "first param is $param1"
+    echo "second param is $param2"
+}
+
+test1 "hello" "world"
