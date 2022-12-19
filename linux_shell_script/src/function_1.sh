887ab6aa405591daa27109075bcbec0d85dcec11
cch0807 <ckdgus11021@gmail.com>
2022-12-19 23:01:56 +0900

    20221219_1

--- linux_shell_script/src/function_1.sh
+++ linux_shell_script/src/function_1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+test1 # 호출 불가
+
 function test1()
 {
     echo "haha. this is test1"
 }
+
+test1 # 호출 가능
 
 function test2
 {
@@ -14,3 +18,8 @@ test3()
 {
     echo "haha. this is test3"
 }
+
+echo "start..."
+test1 # test1 함수 실행
+test2 # test2 함수 실행
+test3 # test3 함수 실행
